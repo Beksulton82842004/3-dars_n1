9e00d5c5276721f74d75437922748ccc5d8fcfc8
Beksulton Po'latov <beksulton753@gmail.com>
2023-07-26 13:49:08 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let samalyot = 500 * 11000.34
+mehmonxona = 250 * 11000.34
+muzey = 120 * 12354
+let pul
+let result = samalyot + mehmonxona + muzey
+console.log(result + " so'm")
+
+ pul = prompt("Qancha pulingiz bor")
+
+if(pul >= result){
+    console.log("Oq yo'l, Alisher!")
+} else  if(result <= pul){console.log("Alisher, ozgina sabr qilish kerak bo'lar ekan.")}
